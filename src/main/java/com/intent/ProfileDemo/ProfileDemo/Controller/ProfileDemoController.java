cddde71f0069855f1dd96a91f80a64b919dc984f
janmaijay1997 <30903817+janmaijay1997@users.noreply.github.com>
2021-05-08 22:23:01 +0530

    implemented Logger with console

--- src/main/java/com/intent/ProfileDemo/ProfileDemo/Controller/ProfileDemoController.java
+++ src/main/java/com/intent/ProfileDemo/ProfileDemo/Controller/ProfileDemoController.java
@@ -1,6 +1,8 @@
 package com.intent.ProfileDemo.ProfileDemo.Controller;
 
 import com.intent.ProfileDemo.ProfileDemo.Config.BasicConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import java.util.Map;
 
 @RestController
 public class ProfileDemoController {
+    private static final Logger LOGGER= LogManager.getLogger(ProfileDemoController.class);
     @Value("${welcome.message}")
     private String welcomeMessage;
 
@@ -19,17 +22,22 @@ public class ProfileDemoController {
 
     @GetMapping("/dynamic-configuration")
     public Map dynamicConfiguration() {
+        LOGGER.info(" dynamicConfiguration initiated ");
         // Not the best practice to use a map to store differnt types!
         Map map = new HashMap();
         map.put("message", configuration.getMessage());
         map.put("number", configuration.getNumber());
         map.put("key", configuration.isValue());
+        LOGGER.info("operation completed");
+
         return map;
     }
 
     @GetMapping("/welcome")
     public String retrieveWelcomeMessage() {
         // Complex Method
+        LOGGER.info("Welcome api  initiated ");
+
         return welcomeMessage;
     }
 
--- src/main/java/com/intent/ProfileDemo/ProfileDemo/ProfileDemoApplication.java
+++ src/main/java/com/intent/ProfileDemo/ProfileDemo/ProfileDemoApplication.java
@@ -1,5 +1,8 @@
 package com.intent.ProfileDemo.ProfileDemo;
 
+import com.intent.ProfileDemo.ProfileDemo.Controller.ProfileDemoController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,8 +11,10 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ProfileDemoApplication {
+	private static final Logger LOGGER= LogManager.getLogger(ProfileDemoApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("application has stated");
 		ApplicationContext applicationContext=SpringApplication.run(ProfileDemoApplication.class, args);
 
 		for (String name : applicationContext.getBeanDefinitionNames()) {
